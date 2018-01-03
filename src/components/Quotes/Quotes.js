e93723cc9fd60e292433bba9fa4d3e72ac483c73
Ting Wang <felix.wang.1026@gmail.com>
2018-01-02 23:55:21 -0500

    Change the color api address

--- src/components/Quotes/Quotes.js
+++ src/components/Quotes/Quotes.js
@@ -21,7 +21,7 @@ class Quotes extends Component {
 
   fetchColor = () => {
     $.ajax({
-      url: "http://www.colr.org/json/color/random",
+      url: "https://www.colr.org/json/color/random",
       dataType: "json"
     })
       .done(response => {
