ce43140d3b43e260ab2d18fe02b8979dce3c08fc
Ting Wang <felix.wang.1026@gmail.com>
2018-04-11 00:15:30 -0400

    refactor the code

--- src/components/Quotes/Quotes.js
+++ src/components/Quotes/Quotes.js
@@ -1,16 +1,16 @@
-import React, { Component } from "react";
-import $ from "jquery";
-import { Div, H1, Button, FlexWrapper, Wrapper } from "./Quote.style";
+import React, { Component } from 'react';
+import $ from 'jquery';
+import { Div, H1, Button, FlexWrapper, Wrapper } from './Quote.style';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import { faQuoteLeft, faQuoteRight } from '@fortawesome/fontawesome-free-solid';
 import { faGithub } from '@fortawesome/fontawesome-free-brands';
 
 class Quotes extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       quote: [],
-      color: ""
+      color: ''
     };
   }
 
@@ -20,19 +20,21 @@ class Quotes extends Component {
   }
 
   fetchColor = () => {
-    const randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    const randomColor = '#000000'.replace(/0/g, () => {
+      return (~~(Math.random() * 16)).toString(16);
+    });
     this.setState({ color: randomColor });
   };
 
   fetchQuote = () => {
     $.ajax({
-      url: "https://api.forismatic.com/api/1.0/",
-      jsonp: "jsonp",
-      dataType: "jsonp",
+      url: 'https://api.forismatic.com/api/1.0/',
+      jsonp: 'jsonp',
+      dataType: 'jsonp',
       data: {
-        method: "getQuote",
-        lang: "en",
-        format: "jsonp"
+        method: 'getQuote',
+        lang: 'en',
+        format: 'jsonp'
       }
     })
       .done(response => {
@@ -60,26 +62,41 @@ class Quotes extends Component {
 
   render() {
     document.body.setAttribute(
-      'style', `background: ${this.state.color}; transition: background 2s ease;`
+      'style',
+      `background: ${this.state.color}; transition: background 2s ease;`
     );
 
     const style1 = this.state.color;
-    const style2 = { margin: "2rem" };
+    const style2 = { margin: '2rem' };
 
     return (
       <Wrapper color={this.state.color}>
         <H1>Random Quote</H1>
         <Div>
           <div>
-            <p><FontAwesomeIcon icon={faQuoteLeft} /> {this.state.quote.quoteText}   <FontAwesomeIcon icon={faQuoteRight} /></p>
+            <p>
+              <FontAwesomeIcon icon={faQuoteLeft} />{' '}
+              {this.state.quote.quoteText}{' '}
+              <FontAwesomeIcon icon={faQuoteRight} />
+            </p>
             <h3>{this.state.quote.quoteAuthor}</h3>
           </div>
           <FlexWrapper>
-            <Button color={this.state.color} onClick={this.handleClick}>Quote Me</Button>
-            <Button color={this.state.color} onClick={this.handleTweet}>Tweet Me</Button>
+            <Button color={this.state.color} onClick={this.handleClick}>
+              Quote Me
+            </Button>
+            <Button color={this.state.color} onClick={this.handleTweet}>
+              Tweet Me
+            </Button>
           </FlexWrapper>
         </Div>
-        <a href="https://github.com/nunulong/randomQuote"><FontAwesomeIcon icon={faGithub} size="3x" style={Object.assign({}, style1, style2)} /></a>
+        <a href="https://github.com/nunulong/randomQuote">
+          <FontAwesomeIcon
+            icon={faGithub}
+            size="3x"
+            style={Object.assign({}, style1, style2)}
+          />
+        </a>
       </Wrapper>
     );
   }
