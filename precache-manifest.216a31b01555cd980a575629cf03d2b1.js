8637594fbffc1ede2e2e6c2122f361af1a9aa0b0
Ting Wang <ting.wang@optum.com>
2020-05-16 00:50:26 -0400

    Updates

---
+++ precache-manifest.216a31b01555cd980a575629cf03d2b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b49bdba944f1964d672ee31b86ab0e",
+    "url": "/randomQuote/index.html"
+  },
+  {
+    "revision": "ef6e421ff2e1b6f34374",
+    "url": "/randomQuote/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "6344d3af338c61fef3b4",
+    "url": "/randomQuote/static/js/2.6ff4d138.chunk.js"
+  },
+  {
+    "revision": "c2cea22a89be62be2002a745548624e1",
+    "url": "/randomQuote/static/js/2.6ff4d138.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef6e421ff2e1b6f34374",
+    "url": "/randomQuote/static/js/main.f4de5ec0.chunk.js"
+  },
+  {
+    "revision": "e6b1b47989b6b44b7d86",
+    "url": "/randomQuote/static/js/runtime-main.7c44f850.js"
+  }
+]);
